5b42a4fbae5c9e11e66a272587ccc8b17efb75a7
swapnilpatil94 <swapnilp576p@gmail.com>
2020-10-11 20:01:52 +0530

    [actionTypes] redux action types

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+export const LOGOUT = 'LOGOUT';
+
+export const SENDMSG_SUCCESSFUL ="SENDMSG_SUCCESSFUL";
+export const SENDMSG_FAIL = 'SENDMSG_FAIL';
