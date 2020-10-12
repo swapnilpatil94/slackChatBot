670dddb0b8cd56e3d3e257def59233340ae6b957
swapnilpatil94 <swapnilp576p@gmail.com>
2020-10-12 11:04:06 +0530

    [TaskList] Add tasklist of schedule msg component

--- src/Components/Main/TaskList.jsx
+++ src/Components/Main/TaskList.jsx
@@ -8,6 +8,14 @@ import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import CountdownTimer from "react-component-countdown-timer";
 import moment from 'moment';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Paper from '@material-ui/core/Paper';
+import { connect } from 'react-redux';
+import {deleteMsg} from '../../actions/deleteMsg'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import DeleteIcon from '@material-ui/icons/Delete';
 const useStyles = makeStyles((theme) => ({
@@ -32,13 +40,25 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function TaskList(props) {
+const TaskList= (props)=> {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [delData, setDelData] = React.useState();
+
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
+
+  const handleClose =()=>{
+    setOpen(false)
+  }
+  const handleDelete =()=>{
+    props.deleteMsg(delData)
+    console.log('det',delData)
+    setOpen(false)
+  }
 
   return (
     <div className={classes.root}>
@@ -57,14 +77,56 @@ export default function TaskList(props) {
         <AccordionDetails>
           <Typography>
           Channel:      { props.channelName} 
-  </Typography> 
+        </Typography> 
 
-        <Box className={classes.delBox}>
+        <Box className={classes.delBox} onClick={()=>{
+            // console.log('k',props.delKey)
+            let data ={
+              id :props.delKey,
+              channel_id: props.channel_id
+            }
+          setOpen(true)
+            // props.deleteMsg(data)
+            setDelData(data)
+
+        }}>
            <Button> <DeleteIcon className={classes.delete} /> </Button>
         </Box>
         </AccordionDetails>
       </Accordion>
       
+
+
+      {/* Dialog */}
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="draggable-dialog-title"
+      >
+        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+          Delete Message
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+         Do you really want to delete messages?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDelete} color="primary">
+            yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+
+
+
     </div>
   );
 }
+
+export default connect(null,{deleteMsg})(TaskList);
